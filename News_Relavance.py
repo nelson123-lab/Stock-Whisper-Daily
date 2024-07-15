181f3b0f98228ae62bd0bcc8dbc69ba3ffbc38a2
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-07-14 23:50:12 -0500

    Updated codebase for news extractor

--- News_Relavance.py
+++ News_Relavance.py
@@ -4,14 +4,14 @@ from openai import OpenAI
 
 load_dotenv()
 
-news_text = """WASHINGTON (AP) — Joe Biden faced a test Thursday that he had avoided so far this year — a solo news conference with questions from the White House press corps.
+# news_text = """WASHINGTON (AP) — Joe Biden faced a test Thursday that he had avoided so far this year — a solo news conference with questions from the White House press corps.
 
-The news conference was meant to reassure a disheartened group of Democratic lawmakers, allies and persuadable voters in this year’s election that Biden still has the strength and stamina to be president. Biden has tried to defend his feeble and tongue-tied performance in the June 27 debate against Republican Donald Trump as an outlier rather than evidence that at 81 he lacks the vigor and commanding presence that the public expects from the commander in chief.
+# The news conference was meant to reassure a disheartened group of Democratic lawmakers, allies and persuadable voters in this year’s election that Biden still has the strength and stamina to be president. Biden has tried to defend his feeble and tongue-tied performance in the June 27 debate against Republican Donald Trump as an outlier rather than evidence that at 81 he lacks the vigor and commanding presence that the public expects from the commander in chief.
 
-He made at least two notable flubs, referring at an event beforehand to Ukrainian President Volodymyr Zelenskyy as “President Putin” and then calling Kamala Harris “Vice President Trump” when asked about her by a reporter. But he also gave detailed responses about his work to preserve NATO and his plans for a second term. And he insisted he’s not leaving the race even as a growing number of Democratic lawmakers ask him to step aside.
-"""
+# He made at least two notable flubs, referring at an event beforehand to Ukrainian President Volodymyr Zelenskyy as “President Putin” and then calling Kamala Harris “Vice President Trump” when asked about her by a reporter. But he also gave detailed responses about his work to preserve NATO and his plans for a second term. And he insisted he’s not leaving the race even as a growing number of Democratic lawmakers ask him to step aside.
+# """
 
-news_title = """Key takeaways from Biden’s news conference: Insistence on staying in the race and flubbed names"""
+# news_title = """Key takeaways from Biden’s news conference: Insistence on staying in the race and flubbed names"""
 
 def check_news_relevance(news_text):
     openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -37,5 +37,5 @@ def news_summarizer(news_content, news_title):
     )
     return response.choices[0].message.content
 
-print(check_news_relevance(news_text))
-print(news_summarizer(news_content = news_text, news_title = news_title))
+# print(check_news_relevance(news_text))
+# print(news_summarizer(news_content = news_text, news_title = news_title))
--- news_extracter_api.py
+++ news_extracter_api.py
@@ -8,7 +8,7 @@ from datetime import date
 import uuid
 import os
 from News_Relavance import check_news_relevance, news_summarizer
-from sentiment_score import sentiment_score
+from sentiment_score import get_sentiment_scores
 
 
 def runner():
@@ -52,13 +52,25 @@ def runner():
             'content': [trimmed_part], 'source': [source], 'author': [author], 'urlToImage': [urlToImage]
         })], ignore_index=True)
 
+    # checking whether the news are relavant or not.
+    df['News_relavance'] = df['content'].apply(check_news_relevance)
+
+    # Keeping only the relavant news related to USA stock market.
+    data = df[df['News_relavance'] == 'Relevant']
+
+    # Providing the sentiment score for each of the news.
+    data['Sentiment_scores'] = data['content'].apply(get_sentiment_scores)
+
+    # Providing the sentiment score for each of the news.
+    data['Summary'] = data.apply(lambda row: news_summarizer(row['content'], row['newsTitle']), axis=1)
 
     # filename = str(uuid.uuid4())
     # output_file = "/home/ubuntu/{}.parquet".format(filename)
-    # df1 = df.drop_duplicates()
     # df1.to_parquet(output_file)
 
     # return output_file
-    return df
+    return data
+
 df = runner()
 df.to_csv("Stock_news.csv")
+print(df.head(5))
